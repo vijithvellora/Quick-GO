04f72369b18f5fd36186b28fffcde2629be6f9d3
Vijith PV <53208145+vijithvellora@users.noreply.github.com>
2023-10-19 19:02:25 +0530

    Update go.sh
    file name update

--- go.sh
+++ go.sh
@@ -4,7 +4,7 @@ cd /tmp &&
 sudo apt-get update  &&
 sudo apt-get -y upgrade  &&
 wget https://dl.google.com/go/go1.21.3.linux-amd64.tar.gz &&
-sudo tar -xvf go1.16.4.linux-amd64.tar.gz &&
+sudo tar -xvf go1.21.3.linux-amd64.tar.gz &&
 sudo mv go /usr/local &&
 export GOROOT=/usr/local/go &&
 cd $HOME && mkdir go &&
