3659e5fa6af90b61c81a0074e0b98ecf6b97b0e3
Vijith PV <53208145+vijithvellora@users.noreply.github.com>
2023-10-19 19:08:17 +0530

    Update go.sh

--- go.sh
+++ go.sh
@@ -3,8 +3,8 @@
 cd /tmp &&
 sudo apt-get update  &&
 sudo apt-get -y upgrade  &&
-wget https://dl.google.com/go/go1.21.3.linux-amd64.tar.gz &&
-sudo tar -xvf go1.21.3.linux-amd64.tar.gz &&
+wget https://dl.google.com/go/go1.20.linux-amd64.tar.gz &&
+sudo tar -xvf go1.20.linux-amd64.tar.gz &&
 sudo mv go /usr/local &&
 export GOROOT=/usr/local/go &&
 cd $HOME && mkdir go &&
@@ -12,5 +12,4 @@ export GOPATH=$HOME/go &&
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH &&
 echo "export PATH=$GOPATH/bin:$GOROOT/bin:$PATH" >> ~/.profile &&
 source ~/.profile &&
-go version 
-go env
+go version
